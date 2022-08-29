38703c64c5b1f48999eb3c1f45bf819fa86c3f98
Akash Karle <74725191+akashkarle10@users.noreply.github.com>
2022-08-29 10:59:10 +0530

    Conditional Rendering Done

--- src/Component/ConditionalRendering.js
+++ src/Component/ConditionalRendering.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 
 class ConditionalRendering extends Component {
     constructor(props) {
-      super(props)
-    
-      this.state = {
-         isLoggedIn:true
-      }
+        super(props)
+
+        this.state = {
+            isLoggedIn: true
+        }
     }
-  render() {
+    render() {
     // First Method 
         // if (this.state.isLoggedIn) {
         //     return (
@@ -22,16 +22,26 @@ class ConditionalRendering extends Component {
         // }
 
     // Second 
-    let message
-    if(this.state.isLoggedIn){
-        message="Hello Akash"
+        // let message
+        // if(this.state.isLoggedIn){
+        //     message="Hello Akash"
+        // }
+        // else{
+        //     message="Hello Guest"
+        // }
+        // return(<h1>{message}</h1>)
+
+    // Third
+        // return this.state.isLoggedIn ?(
+        // <h1>Hello Akash</h1>
+        // ):(
+        // <h1>Hello Guest</h1>
+        // )
+
+    // Fourth
+        return this.state.isLoggedIn && <h1>Hello Akash</h1>
     }
-    else{
-        message="Hello Guest"
-    }
-    return(<h1>{message}</h1>)
-  }
-  
+
 }
 
 export default ConditionalRendering
